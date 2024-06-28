adaa6f337d5499edd8a031fd09a38fcb0e5677c6
janmejoy <janmejoysarker@gmail.com>
2024-06-28 23:07:39 +0530

    Report updated. Testing performed on Dust Removal from PRNU

--- src/dust_removal_LED_based.py
+++ src/dust_removal_LED_based.py
@@ -16,6 +16,8 @@ from astropy.convolution import convolve, Box2DKernel
 from skimage.morphology import dilation, disk
 from scipy.ndimage import median_filter
 from concurrent.futures import ProcessPoolExecutor
+import matplotlib.pyplot as plt
+import glob
 
 def lighten(image_list):
     '''
@@ -30,12 +32,9 @@ def lighten(image_list):
                     lighten_blend[i,j]=image[i,j]
     return(lighten_blend)
 
-def dust_mask(): #make a mask of all dust from PRNU image
-    prnu_path= os.path.expanduser('~/Dropbox/Janmejoy_SUIT_Dropbox/flat_field/LED/onboard_PRNU_project/')
-    prnu1= fits.open(prnu_path+'data/processed/2024-05-17_prnu_355_ff.fits')[0].data
-    prnu2= fits.open(prnu_path+'data/processed/2024-05-17_prnu_355_aa.fits')[0].data
-    lighten_blend= lighten([prnu1, prnu2])
-    blurred= convolve(lighten_blend, Box2DKernel(4), normalize_kernel=True)
+def dust_mask(prnu_path): #make a mask of all dust from PRNU image
+    prnu= fits.open(prnu_path)[0].data
+    blurred= convolve(prnu, Box2DKernel(4), normalize_kernel=True)
     mask= dilation(blurred<0.99, disk(3))
     print("Mask Generated")
     return (mask)
@@ -44,22 +43,40 @@ def filter_image(image_path): #main process to remove dust spots
     hdu= fits.open(image_path)[0]
     print(f'{image_path[-64:]} read')
     data, header= hdu.data, hdu.header
-    mask= dust_mask()
+    mask= dust_mask(prnu_path)
     med= median_filter(data, footprint=disk(10))
     filtered= (data*(np.invert(mask)))+(med*mask)
     if save==True: 
         header['COMMENT']=('Dust Corrected', 'Comment')
         sav_hdu= fits.PrimaryHDU(filtered, header=header)
         sav_hdu.writeto(os.path.join(sav, image_path[-64:]), overwrite=True)
-    return(filtered)
+    return (data, filtered)
+
+def plot(data, filtered):
+    plt.figure()
+    plt.subplot(1,2,1)
+    plt.imshow(data,vmin=0, vmax=5e4, origin='lower')
+    plt.title("Raw Image")
+    plt.subplot(1,2,2)
+    plt.imshow(filtered, vmin=0, vmax=5e4, origin='lower')
+    plt.title("Dust Filtered")
+    plt.show()
         
 if __name__=='__main__':
-    save=True
-    sav= os.path.expanduser('~/Dropbox/Janmejoy_SUIT_Dropbox/flat_field/morphological_dust_removal_project/products')
-    img_folder='/home/janmejoyarch/Dropbox/Janmejoy_SUIT_Dropbox/flat_field/morphological_dust_removal_project/data/raw/'
-    file_list=['SUT_C24_0302_000379_Lev0.5_2024-05-17T05.24.11.637_4081NB08.fits']
-    
-    path_list= [os.path.join(img_folder, file) for file in file_list]
+    save=False
+    project_path= os.path.expanduser('~/Dropbox/Janmejoy_SUIT_Dropbox/flat_field/morphological_dust_removal_project/')
+    prnu_path= os.path.join(project_path, 'data/external/2023-11-23_prnu_355_common.fits')
+    sav= os.path.join(project_path, 'products')
+    file_list=glob.glob(os.path.join(project_path, 'data/raw/*'))
+    data, filtered= filter_image(file_list[0])
+    plot(data, filtered)
 
+    x,y,s= 1366, 1393, 25
+    data_crop, filtered_crop= data[y-s:y+s, x-s:x+s], filtered[y-s:y+s, x-s:x+s]
+    print(np.sum(data_crop), np.sum(filtered_crop))
+    plot(data_crop, filtered_crop)
+    
+'''
     with ProcessPoolExecutor() as executor:
-        executor.map(filter_image, path_list)
+        executor.map(filter_image, file_list)
+'''
